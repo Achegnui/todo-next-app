fcf8af6e1176cc36948e98438549269a84a2e69c
Achegnui <achegnuijuven@gmail.com>
2024-08-08 13:51:58 +0100

    UI completed now backend in process

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,52 @@
+"use client";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Todo from "@/Components/Todo";
+import { useState } from "react";
 
 export default function Home() {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
+
+  const onChangeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormData((form) => ({ ...form, [name]: value }));
+    console.log(formData);
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      //api code
+
+      toast.success("Success");
+    } catch (error) {
+      toast.error("Error");
+    }
+  };
+
   return (
     <>
-      <form className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto">
+      <ToastContainer theme="dark" />
+      <form
+        onSubmit={onSubmitHandler}
+        className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto"
+      >
         <input
+          value={formData.title}
+          onChange={onChangeHandler}
           type="text"
           name="title"
           placeholder="Enter Title"
           className="px-3 py-2 border w-full"
         />
         <textarea
+          value={formData.description}
+          onChange={onChangeHandler}
           name="description"
-          id=""
           placeholder="Enter description"
           className="px-3 py-2 border-2 w-full"
         ></textarea>
@@ -43,6 +77,8 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
+            <Todo />
+            <Todo />
             <Todo />
           </tbody>
         </table>
