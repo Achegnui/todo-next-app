93978022ab9398d13937c5bb89bd0d06f7b5fb79
Achegnui <achegnuijuven@gmail.com>
2024-08-08 12:16:30 +0100

    Dummy  structure for to do made with the todo component added in the page.jsx file

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Todo from "@/Components/Todo";
+
 export default function Home() {
   return (
     <>
@@ -41,17 +43,7 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Apple MacBook Pro 17
-              </th>
-              <td className="px-6 py-4">Silver</td>
-              <td className="px-6 py-4">Laptop</td>
-              <td className="px-6 py-4">$2999</td>
-            </tr>
+            <Todo />
           </tbody>
         </table>
       </div>
